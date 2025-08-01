fd637654e978ac2f8cd6634a6ef1d4eca8c5f665
Kastarov <kastarovalibek11@gmail.com>
2025-08-01 04:55:14 -0700

    Add files via upload

---
+++ telebot aiogram/bot7.py
@@ -0,0 +1,25 @@
+import asyncio  # для асинхронности
+import logging  # для логирования
+from aiogram import Bot, Dispatcher, types  # для бота
+from aiogram.filters.command import Command
+
+# Включаем логирование, чтобы не пропустить важные сообщения
+logging.basicConfig(level=logging.INFO)
+
+# Объект бота
+bot = Bot(token="7625463573:AAGN4DB01C3g4GzWqYnoNsRsy-tb8XEWY_k")
+
+# Диспетчер
+dp = Dispatcher()
+
+# Хэндлер на команду /start
+@dp.message(Command("start"))
+async def cmd_start(message: types.Message):
+    await message.answer("Hello!")
+
+# Запуск процесса поллинга новых апдейтов
+async def main():
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
